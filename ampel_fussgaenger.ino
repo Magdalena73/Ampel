d661405df4f6dd117df9fe2528ff95ad98da7275
Magdalena73 <magdalena73@gmx.de>
2019-08-22 20:45:47 +0200

    Add files via upload

---
+++ ampel_fussgaenger.ino
@@ -0,0 +1,48 @@
+int rot =  12;  /* benutzt pin 12 für die rote LED - Autoampel.*/
+int gelb = 11;  /* benutzt pin 11 für die gelbe LED - Autoampel.*/
+int gruen = 10; /* benutzt pin 10 für die grüne LED - Autoampel.*/
+int f_rot = 9;  /* benutzt pin 9 für die rote LED - Fußgängerampel.*/
+int f_gr = 8;   /* benutzt pin 8 für die grüne LED - Fußgängerampel.*/
+int taste = 2;  /* benutzt pin 2 für den Taster - Fußgängerampel Knopf.*/
+int PauseKurz = 700;
+int PauseLang = 6000;
+
+void setup()  /* initialisiert die pins 8 bis 12 als Ausgang 
+                  und pin 2 als Eingang für den Taster*/
+{ 
+  pinMode(rot, OUTPUT);
+  pinMode(gelb, OUTPUT);
+  pinMode(gruen, OUTPUT);
+  pinMode(f_rot, OUTPUT);
+  pinMode(f_gr, OUTPUT);
+  pinMode(taste, INPUT_PULLUP);
+}
+
+void loop()  /*Fortlaufende Schleife*/
+{ /* am Anfang leuchtet die Autoampel grün und die Fußgängerampel rot 
+      bis jemand den Taster druckt */
+  digitalWrite(gruen, HIGH);
+  digitalWrite(f_rot, HIGH);
+  if (digitalRead(taste) == HIGH) /* wenn jemand den Taster drückt, passiert folgendes:  */
+  {
+    digitalWrite(gruen, LOW); /* Autoampel schaltet die grüne LED aus.*/
+    digitalWrite(gelb, HIGH); /* Autoampel schaltet die gelbe LED an.*/
+    delay(PauseKurz);               /* wartet 0,6 Sekunden */
+    digitalWrite(gelb, LOW);  /* Autoampel schaltet die gelbe LED aus.*/ 
+    digitalWrite(rot, HIGH);  /* Autoampel schaltet die rote LED an.*/
+    delay(PauseKurz);               /* wartet 0,6 Sekunden */
+    digitalWrite(f_rot, LOW); /* Fußgängerampel schaltet die rote LED aus.*/
+    digitalWrite(f_gr, HIGH); /* Fußgängerampel schaltet die grüne LED an.*/
+    delay(PauseLang);              /* wartet 5 Sekunden */
+    digitalWrite(f_gr, LOW); /* Fußgängerampel schaltet die grüne LED aus.*/
+    digitalWrite(f_rot, HIGH);  /* Fußgängerampel schaltet die rote LED an.*/
+    delay(PauseKurz);               /* wartet 0,6 Sekunden */
+    digitalWrite(gelb, HIGH);
+    delay(PauseKurz);               /* wartet 0,6 Sekunden */
+    digitalWrite(rot, LOW);   /* Autoampel schaltet die rote LED aus.*/ 
+    digitalWrite(gelb, LOW);  /* Autoampel schaltet die gelbe LED aus.*/
+    digitalWrite(gruen, HIGH);  /* Autoampel schaltet die grüne LED an.*/
+    delay(2000);              /* wartet 2 Sekunden und fängt von vorne an */
+  }
+}
+
